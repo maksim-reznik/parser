fc3955d68526a31a1efb1def70c9c6e4b7a56f63
maksim-reznik <77396857+maksim-reznik@users.noreply.github.com>
2022-05-17 15:55:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,4 +19,20 @@ authdata = {'username': '89296617898', 'password': '12345678'}
 mysession = requests.session()
 response = mysession.post('https://erp.uchi.ru/admin/login', data=authdata).text
 req = bs4.BeautifulSoup(response, 'html.parser')
-print(response)
+
+
+resp = mysession.get(html, data=authdata).text
+print(resp)
+
+
+
+
+# resp = mysession.post('https://erp.uchi.ru/admin/call-request/admin/request', data=authdata).text
+# req = bs4.BeautifulSoup(resp, 'html.parser')
+# print(resp)
+
+
+# mysession = requests.session()
+# response = mysession.post('https://erp.uchi.ru/admin/login', data=authdata).text
+# soup = BeautifulSoup(response.text,"lxml")
+# print(soup)
